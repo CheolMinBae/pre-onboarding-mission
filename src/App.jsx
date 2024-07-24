b566dcdf1a2a1f8d7ba2684b3f27a0021fe40b78
Jun Seong <drawbeat0112@gmail.com>
2024-07-24 18:55:52 +0900

    style, 기본골조 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { FaSearch } from "react-icons/fa";
+import { dummy } from "./data";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="container">
+      <div className="input-container">
+        <input type="text" placeholder="검색어를 입력해주세요" />
+        <ul>
+          {dummy.map((el) => (
+            <li key={el.key}>{el.description}</li>
+          ))}
+        </ul>
+      </div>
+      <button>
+        <FaSearch />
+      </button>
+    </div>
   );
 }
 
