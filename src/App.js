02ee358381a48b897831774a5664e0707ef58813
sonjieun <wkaqh24@naver.com>
2024-07-25 22:33:55 +0900

    Set: 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchBar from './components/SearchBar';
+
+function App() {
+    return (
+        <div className="App">
+            <SearchBar />
+        </div>
+    );
+}
+
+export default App;
