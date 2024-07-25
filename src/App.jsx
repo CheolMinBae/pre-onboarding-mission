c7b904c56a0a688d50206d8c72d80f2ccd954559
Jun Seong <drawbeat0112@gmail.com>
2024-07-25 13:22:11 +0900

    해당되는 description bold 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import { FaSearch } from "react-icons/fa";
 import { dummy } from "./data";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <div className="container">
       <div className="input-container">
-        <input type="text" placeholder="검색어를 입력해주세요" />
+        <input
+          type="text"
+          placeholder="검색어를 입력해주세요"
+          value={value}
+          onChange={handleChange}
+        />
         <ul>
           {dummy.map((el) => (
-            <li key={el.key}>{el.description}</li>
+            <li
+              key={el.key}
+              className={
+                value && el.description.includes(value) ? "highlight" : ""
+              }
+            >
+              {el.description}
+            </li>
           ))}
         </ul>
       </div>
