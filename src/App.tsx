d7d86439b790c2600e7bf3758c482c0955e2dbb0
LeeDahee <dduosy9@gmail.com>
2024-07-25 23:41:58 +0900

    feat: 검색창 생성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { FcSearch } from 'react-icons/fc';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [search, setSearch] = useState<string>('');
+  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={search} onChange={handleChangeSearch} />
+      <button type="submit" aria-label="검색 버튼">
+        <FcSearch />
+      </button>
     </>
   );
 }
