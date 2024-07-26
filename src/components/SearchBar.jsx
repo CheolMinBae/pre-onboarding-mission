546f1a0b8a1e241f6b195387195a00adee7eebb8
sonjieun <wkaqh24@naver.com>
2024-07-27 02:58:18 +0900

    Feat: 입력시 리스트 보이도록

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,58 @@
+import { useState } from 'react';
+
+import '../SearchBar.css';
+import { dummy } from '../data';
+import groupByType from '../utils/groupByType';
+
 const SearchBar = () => {
-    return <></>;
+    const [searchTerm, setsearchTerm] = useState('');
+
+    const groupedData = groupByType(dummy);
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setsearchTerm(value);
+    };
+
+    const splitIntoChars = (str) => {
+        const chars = str.split('');
+
+        return (
+            <div>
+                {chars.map((char, index) => (
+                    <span key={index}>{char}</span>
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <div className="search-bar">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
+                className="search-input"
+            />
+            {searchTerm && (
+                <div className="search-results">
+                    {Object.entries(groupedData).map(([group, value]) => (
+                        <div>
+                            <div className="group">{group}</div>
+                            <ul className="group-list">
+                                {value.map((item) => (
+                                    <li key={item.key} className="group-item">
+                                        {splitIntoChars(item.description)}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            )}
+            <button type="button">검색</button>
+        </div>
+    );
 };
 
 export default SearchBar;
