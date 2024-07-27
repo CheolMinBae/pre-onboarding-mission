914887d66b0ffb2070465432a958fd754a52bb07
te-ing <hi2177@naver.com>
2024-07-27 14:30:01 +0900

    feat: 검색어 입력 시 볼드처리 하는 검색어 추천박스 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import { ChangeEventHandler, useState } from 'react';
-import './App.css';
+import S from './App.module.css';
 import { dummy } from './data.ts';
 
 function App() {
   const [search, setSearch] = useState('');
-  console.log(dummy);
+
   const handleSearchInput: ChangeEventHandler<HTMLInputElement> = e => {
     setSearch(e.target.value);
   };
 
+  const highlightWords = (text: string, word: string) => {
+    const parts = text.split(new RegExp(`(${word})`, 'gi'));
+    return (
+      <>
+        {parts.map((part, index) =>
+          part.toLowerCase() === word.toLowerCase() ? (
+            <b key={index}>{part}</b>
+          ) : (
+            part
+          )
+        )}
+      </>
+    );
+  };
+
   return (
-    <form>
-      <input value={search} onChange={handleSearchInput} />
-      <button>🔍</button>
-    </form>
+    <main>
+      <form>
+        <input
+          className={S.input}
+          value={search}
+          onChange={handleSearchInput}
+        />
+        <button>🔍</button>
+      </form>
+      {search && (
+        <div className={S.recommend}>
+          <ul>
+            <li className={S.listLabel}>COMPANY</li>
+            {dummy.map(({ key, description }) => {
+              return <li key={key}>{highlightWords(description, search)}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+    </main>
   );
 }
-
 export default App;
