a624a7af70964aa643387540ff99000fcfc0fbf6
jnkeniaem <jeanne.kim9@gmail.com>
2024-07-29 20:59:40 +0900

    type도 list로 추가

--- SearchProgram/src/App.jsx
+++ SearchProgram/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { dummy } from "./assets/data";
 
@@ -6,8 +6,16 @@ function App() {
   const [searchList, setSearchList] = useState(dummy);
   const [searchKeyword, setSearchKeyword] = useState("");
 
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      console.log("hi");
+    }, 333);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchKeyword]);
+
   const onChange = (e) => {
-    console.log("hi");
     setSearchKeyword(e.target.value);
   };
 
@@ -17,9 +25,22 @@ function App() {
       {searchKeyword && (
         <div id="listContainer">
           <ul>
-            {searchList.map((elem) => (
-              <li>{elem.description}</li>
-            ))}
+            {searchList.map((elem, idx) => {
+              console.log(idx == 0, elem.type);
+              return (
+                <>
+                  {(idx == 0 ||
+                    (idx > 0 && searchList[idx - 1].type != elem.type)) && (
+                    <li className="type" key={elem.type}>
+                      {elem.type}
+                    </li>
+                  )}
+                  <li className="notAType" key={idx}>
+                    {elem.description}
+                  </li>
+                </>
+              );
+            })}
           </ul>
         </div>
       )}
