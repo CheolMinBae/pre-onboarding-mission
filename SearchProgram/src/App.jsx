3e04b32736cc17d0d05be5733c9334c0347942d3
jnkeniaem <jeanne.kim9@gmail.com>
2024-07-29 20:19:21 +0900

    css & list hover할때 css 변경

--- SearchProgram/src/App.jsx
+++ SearchProgram/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { dummy } from "./assets/data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchList, setSearchList] = useState(dummy);
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const onChange = (e) => {
+    console.log("hi");
+    setSearchKeyword(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="container">
+      <input onChange={(e) => onChange(e)}></input>
+      {searchKeyword && (
+        <div id="listContainer">
+          <ul>
+            {searchList.map((elem) => (
+              <li>{elem.description}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
