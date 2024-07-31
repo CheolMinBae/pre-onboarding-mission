7a6048e1f56bc470ad25f0a47e1a12db9ecdb170
Dominico Yoon <808dominico@gmail.com>
2024-07-31 17:12:14 +0900

    20240731 - feat: 기능 구현

--- pre-onboarding-mission-1/src/App.jsx
+++ pre-onboarding-mission-1/src/App.jsx
@@ -2,19 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import SearchList from "./components/SearchList";
-import { groupByData } from "../utils/groupByData";
-import { dummy } from "../data/data";
 
 function App() {
   const [searchValue, setSearchValue] = useState();
 
-  const groupData = groupByData(dummy);
-  // console.log(groupData);
-
   return (
     <>
       <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-      {searchValue && <SearchList searchValue={groupData} />}
+      {searchValue && <SearchList searchValue={searchValue} />}
     </>
   );
 }
--- pre-onboarding-mission-1/src/components/SearchList.jsx
+++ pre-onboarding-mission-1/src/components/SearchList.jsx
@@ -1,13 +1,39 @@
+import "./SearchList.css";
 import { dummy } from "../../data/data";
 
-function SearchList() {
-  return (
-    <div>
-      {dummy.map((item) => {
-        return <div key={item.key}>{item.description}</div>;
-      })}
-    </div>
+const searchTextBold = (text, search) => {
+  if (!search) return text;
+
+  const regex = new RegExp(`(${search})`, "gi");
+  const parts = text.split(regex);
+
+  return parts.map((part, index) =>
+    part.toLowerCase() === search.toLowerCase() ? (
+      <b key={index}>{part}</b>
+    ) : (
+      part
+    )
   );
+};
+
+const searchItemList = (search) =>
+  dummy.map((item, index) => {
+    const [cur, before] = [item, dummy[index - 1] || { type: null }];
+
+    return (
+      <div key={index}>
+        {(cur.type !== before.type || index === 0) && (
+          <div className="group">{item.type}</div>
+        )}
+        <div className="item" key={item.key}>
+          {searchTextBold(item.description, search)}
+        </div>
+      </div>
+    );
+  });
+
+function SearchList({ searchValue }) {
+  return <div className="SearchList">{searchItemList(searchValue)}</div>;
 }
 
 export default SearchList;
