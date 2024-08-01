6bdf59e162180244522cdee9e56aadf295da34ab
Hyomin Koo <me@eatsteak.dev>
2024-08-01 18:59:01 +0900

    refactor: layering css to cascade as intended

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import SearchBox from "./components/SearchBox.tsx";
-import searchIcon from "./assets/search.svg";
 import { dummy } from "./data.ts";
 import Button from "./components/Button.tsx";
+import SearchIcon from "./icons/SearchIcon.tsx";
 
 function App() {
   return (
@@ -10,13 +10,15 @@ function App() {
       <h1>pre-onboarding-mission</h1>
       <main>
         <form
+          id="search"
           autoComplete="off"
           aria-autocomplete="list"
           onSubmit={(event) => event.preventDefault()}
+          style={{ width: "100%" }}
         >
-          <SearchBox items={dummy} />
-          <Button>
-            <img src={searchIcon} alt={"Search Icon"} />
+          <SearchBox className="stick-right" items={dummy} />
+          <Button className="stick-left">
+            <SearchIcon />
           </Button>
         </form>
       </main>
