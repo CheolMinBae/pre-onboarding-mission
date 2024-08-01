3f711206c4a8e19c3be2900b1d8926b431c64832
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-02 02:15:51 +0900

    feat: 검색 버튼 추가

--- mission/src/components/index.tsx
+++ mission/src/components/index.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react";
+import searchIcon from "../assets/search.svg";
 import { dummy } from "../data.ts";
 
 const boldText = (text: string, query: string) => {
@@ -32,14 +33,17 @@ const Input = () => {
   };
 
   return (
-    <>
-      <div>
+    <div>
+      <div className="flex gap-1">
         <input
           type="text"
           className="w-60 h-8 border-black border-2 rounded-lg"
           value={text}
           onChange={handleChangeInput}
         />
+        <button>
+          <img src={searchIcon} className="h-8" alt="search icon" />
+        </button>
       </div>
       {text && (
         <div className="w-60 h-[120px] border-2 overflow-scroll">
@@ -62,7 +66,7 @@ const Input = () => {
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
