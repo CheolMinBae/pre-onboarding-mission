6892186fdf92c0d32a0bf15a31606c6692dc45c7
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-01 22:14:38 +0900

    feat: Input 컴포넌트 이벤트 로직 작성

--- mission/src/App.tsx
+++ mission/src/App.tsx
@@ -1,5 +1,5 @@
 import Input from "./components";
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
--- mission/src/components/index.tsx
+++ mission/src/components/index.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const Input = () => {
   const [text, setText] = useState("");
 
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
-      <input></input>
+      <input
+        type="text"
+        className="w-60 h-8 border-black border-2 rounded-lg"
+        value={text}
+        onChange={handleChangeInput}
+      />
     </div>
   );
 };
