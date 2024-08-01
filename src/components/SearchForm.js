97f3514b8fcdb9b016ce7944d08b8a635cced364
Yu-Na Lee <lyn_94@naver.com>
2024-08-01 19:13:27 +0900

    feat: 로직 추가

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { dummy } from "../data";
+import { groupByType } from "../util/group";
 
 const SearchForm = () => {
+  const [inputText, setInputText] = useState("");
+
+  const dropdownBox = () => {
+    const groupedItems = groupByType(dummy);
+    const escapeRegExp = inputText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const regex = new RegExp(`(${escapeRegExp})`, "i");
+
+    return Object.keys(groupedItems).map((type) => (
+      <div key={type} className="flex flex-col w-full text-sm">
+        <div className="bg-purple-200 text-white px-2 ">{type}</div>
+        {groupedItems[type].map((item, index) => {
+          const parts = item.description.split(regex);
+          return (
+            <div
+              key={index}
+              className="flex flex-row w-full px-2"
+              onClick={() => setInputText(parts)}
+            >
+              <p>
+                {parts.map((part, i) =>
+                  regex.test(part) ? <b key={i}>{part}</b> : part
+                )}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    ));
+  };
+
+  const handleSearch = () => {
+    window.alert(`검색어는 ${inputText} 입니다.`);
+  };
+
   return (
-    <div>
-      <input></input>
-      <button>검색하기</button>
+    <div className="flex flex-row w-full gap-2">
+      <div className="w-1/2">
+        <input
+          type="text"
+          className="input input-bordered input-sm focus-within:outline-none w-full"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        {!!inputText.length && (
+          <div className="h-28 overflow-scroll border-[1px] rounded-md mt-1 ">
+            {dropdownBox()}
+          </div>
+        )}
+      </div>
+      <button
+        className="btn btn-sm bg-purple-400 text-white border-none"
+        onClick={handleSearch}
+      >
+        검색
+      </button>
     </div>
   );
 };
---
+++ src/util/group.js
@@ -0,0 +1,9 @@
+export const groupByType = (items) => {
+  return items.reduce((acc, item) => {
+    if (!acc[item.type]) {
+      acc[item.type] = [];
+    }
+    acc[item.type].push(item);
+    return acc;
+  }, {});
+};
