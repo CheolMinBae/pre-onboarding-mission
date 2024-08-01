2e98ddbf46daae0c53455ca190a24b9006c2a447
kmwyatt <arc2437@naver.com>
2024-08-01 14:25:17 +0900

    feat: Show parsed data in search list

--- src/components/search-list.tsx
+++ src/components/search-list.tsx
@@ -1,18 +1,23 @@
+import { parseData } from '@/helpers/parse-data'
+import { dummy } from '@/assets/data'
+
 export default function SearchList() {
+    const parsedData = parseData(dummy)
+
     return (
         <div className="search-list-container">
-            <div className="search-list-label">COMPANY</div>
-            <div className="search-list-item">COMPANY</div>
-            <div className="search-list-item">COMPANY</div>
-            <div className="search-list-label">COUNTRY</div>
-            <div className="search-list-item">COUNTRY</div>
-            <div className="search-list-item">COUNTRY</div>
-            <div className="search-list-label">PEOPLE</div>
-            <div className="search-list-item">PEOPLE</div>
-            <div className="search-list-item">PEOPLE</div>
-            <div className="search-list-label">JOB</div>
-            <div className="search-list-item">JOB</div>
-            <div className="search-list-item">JOB</div>
+            {parsedData.map((category) => (
+                <>
+                    <div className="search-list-label" key={category.type}>
+                        {category.type}
+                    </div>
+                    {category.data.map((item) => (
+                        <div className="search-list-item" key={item.key}>
+                            {item.description}
+                        </div>
+                    ))}
+                </>
+            ))}
         </div>
     )
 }
