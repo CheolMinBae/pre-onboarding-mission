5a123c962eda1b830255918125359b0dfba8d5d7
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-01 22:52:11 +0900

    feat: 검색 드롭다운 데이터 필터

--- mission/src/components/index.tsx
+++ mission/src/components/index.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react";
+import { dummy } from "../data.ts";
 
 const Input = () => {
   const [text, setText] = useState("");
@@ -8,14 +9,28 @@ const Input = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        className="w-60 h-8 border-black border-2 rounded-lg"
-        value={text}
-        onChange={handleChangeInput}
-      />
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          className="w-60 h-8 border-black border-2 rounded-lg"
+          value={text}
+          onChange={handleChangeInput}
+        />
+      </div>
+      {text && (
+        <div className="w-60 h-32 border-2 overflow-scroll">
+          {dummy.map((el, index) => (
+            <div key={el.key}>
+              {(index === 0 || el.type !== dummy[index - 1].type) && (
+                <div>{el.type}</div>
+              )}
+              <div>{el.description}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
