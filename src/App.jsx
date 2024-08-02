f5fe1c36fffaf1ac98a3915b5ad876c12374db22
redzzzi <myqueen1346@naver.com>
2024-08-02 18:56:13 +0900

    feature : 검색창 기능 구현 1. mui 모듈 이용 2. type으로 그룹화 3. 검색어 콘솔창 출력

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBox from "./page/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
