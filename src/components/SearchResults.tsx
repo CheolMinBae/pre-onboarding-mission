aca6932b71b9664379fc187dd49465b51316b637
misobae <misobae0731@gmail.com>
2024-08-02 13:11:07 +0900

    Feat: 디바운스 및 메모이제이션 적용

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { dummy } from "../mocks/dummy";
 
 export interface Results {
@@ -11,28 +12,42 @@ interface SearchResultsProps {
 }
 
 function SearchResults({ keyWord }: SearchResultsProps) {
-  const highlightText = (text: string) => {
-    if (!keyWord) return text;
-    const regex = new RegExp(`(${keyWord})`, 'gi');
+  const [debouncedKeyword, setDebouncedKeyword] = useState(keyWord);
+
+  const highlightText = useCallback((text: string) => {
+    if (!debouncedKeyword) return text;
+    const regex = new RegExp(`(${debouncedKeyword})`, 'gi');
     const parts = text.split(regex);
   
     return parts.map((part, index) => 
       regex.test(part) ? <strong key={index}>{part}</strong> : part
     );
-  };
+  }, [debouncedKeyword]);
 
-  const groupedItems = dummy.reduce<Record<string, Results[]>>((acc, item) => {
-    // 데이터의 type이 없을 시 빈 배열로 초기화
-    if (!acc[item.type]) {
-      acc[item.type] = [];
-    }
-    // 각 타입의 배열에 아이템 추가
-    acc[item.type].push(item);
+  const groupedItems =useMemo(() => {
+    return dummy.reduce<Record<string, Results[]>>((acc, item) => {
+      // 데이터의 type이 없을 시 빈 배열로 초기화
+      if (!acc[item.type]) {
+        acc[item.type] = [];
+      }
+      // 각 타입의 배열에 아이템 추가
+      acc[item.type].push(item);
 
-    return acc;
-  }, {});
+      return acc;
+    }, {});
+  }, []);
 
-  if (!keyWord) return null;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedKeyword(keyWord);
+    }, 250);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [keyWord]);
+
+  if (!debouncedKeyword) return null;
 
   return (
     <>
