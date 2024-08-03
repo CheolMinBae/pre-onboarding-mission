ca07ce0ad8a1c609e8b68d4eeab892f2e566aecf
SUNGWHANYOON <ysh0214727@naver.com>
2024-08-03 23:19:03 +0900

    list 불러오기

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 import {dummy} from './data.js'
 
-let dataListElement = document.querySelector(".data")
-console.log(dataListElement)
+let dataBarElement = document.querySelector(".search_bar")
+let dataListLayout = document.querySelector(".search_list")
+
+dataBarElement.addEventListener('keyup',()=>{
+    dataListLayout.style.display = "block"
+})
 
 dummy.forEach((element)=>{
-    let dataListOption = document.createElement('option')
-    console.log(element)
-    dataListOption.value = element
+    let dataListElement = document.createElement('div')
+    dataListElement.className = ".search_list_element"
 
-    dataListElement.appendChild(dataListOption)
+    dataListElement.style.height = "2rem";
+    dataListElement.style.border = "0.5px solid skyblue";
+    dataListElement.innerText = element.description;
+    dataListLayout.appendChild(dataListElement)
+    
 })
