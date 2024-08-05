dc4e9f2045bbb6d4f26e1ad346ac6070b9fcff8e
JaYoungCho <mkwhwkdud@gmail.com>
2024-08-05 18:42:14 +0900

    Setting: vite변경

---
+++ w-project/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <header className="app-header">
+          <div className="input-area">
+            <input type="text" />
+            <div></div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
