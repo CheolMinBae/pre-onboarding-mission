3e8d2df5f57b8c8302a0ef93aa03832243909105
다홍 <dahhongdu>
2024-08-05 15:25:16 +0900

    decorate: css completed for search input

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import Search from './Search';
 
 function App() {
--- my-app/src/Search.tsx
+++ my-app/src/Search.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
+import { FcSearch } from 'react-icons/fc';
 
 const Search = () => {
   const [inputValue, setInputValue] = useState('');
 
   return (
-    <div>
-      <input
-        name="search"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center gap-2">
+        <input
+          name="search"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="border border-gray-500 rounded px-2 p-3 text-2xl w-7/10 h-100"
+        />
+        <button
+          type="button"
+          className="hover:opacity-80"
+        >
+        <FcSearch size="3rem" />
+        </button>
+      </div>
     </div>
   );
 };
